a2375fb65879199302e126a2063534f79ac1c54d
Janyl <99257368+Janylkalykovna@users.noreply.github.com>
2022-07-07 17:38:58 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       /* Random random = new Random();
+        ArrayList<Integer> numbers = new ArrayList<>();
+        ArrayList<Integer> takSandar = new ArrayList<>();
+        ArrayList<Integer> jupSan = new ArrayList<>();
+
+        for (int i = 0; i < 50; i++) {
+            numbers.add(random.nextInt(1, 100));
+            if (numbers.get(i) % 2 == 1) {
+                takSandar.add(numbers.get(i));
+            } else {
+                jupSan.add(numbers.get(i));
+            }
+        }
+        System.out.println("Random sandar: " + numbers);
+        System.out.println("Tak sandar: " + takSandar);
+        System.out.println("Jup sandar: " + jupSan);
+ }*/
+        Random random =new Random();
+        ArrayList<Integer>onlyNumbers= new ArrayList<>();
+        ArrayList<Integer>evenNumber= new ArrayList<>();
+        ArrayList<Integer>oddNumber= new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            onlyNumbers.add(random.nextInt(1,100));
+
+        } if(onlyNumbers%2==0){
+
+
+        }
+
+    }}
+
